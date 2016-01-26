f984d05a7f59e20eb264009f607c8d4f9496047d
730342 <730342@10.9.30.81>
2016-01-25 20:09:32 -0600

    there

--- robocode2016/src/robocode2016/Properties.java
+++ robocode2016/src/robocode2016/Properties.java
@@ -1,7 +1,7 @@
 package robocode2016;
 
 public class Properties {
-	
+	/*
 	public static Speedcontroller LEFT_MOTOR = new Jaguar(0);
 	public static Speedcontroller RIGHT_MOTOR = new Jaguar(1);
 	
@@ -10,5 +10,5 @@ public class Properties {
 	
 	public static Encoder LEFT_DRIVE = new Encoder(2,3,false,CounterBase.EncodingType.k4x);
 	public static Encoder RIGHT_DRIVE = new Encoder(4,5,false,CounterBase.EncodingType.k4x);
-
+*/
 }
