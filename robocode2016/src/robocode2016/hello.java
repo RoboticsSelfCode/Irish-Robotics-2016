db01f1cf13c998b74ae02f019abf8cb9ec188d8e
730342 <730342@10.9.30.81>
2016-01-25 20:08:21 -0600

    here

--- robocode2016/src/robocode2016/hello.java
+++ robocode2016/src/robocode2016/hello.java
@@ -2,9 +2,6 @@ package robocode2016;
 
 public class hello {
 	public static void main (String args[]) {
-		System.out.println("This was a test to see if the code would add to github");
-		System.out.println("	Sam is a stoop and is broken. "+"\n"+"		Thane is a mess.");
-		System.out.println("			Do not use google docs - Andrew the mentor"	);
-		System.out.println("\n" + "				Error 404: Sam Olson is no longer in use. Try again later."		);
+		
 	}
 }
