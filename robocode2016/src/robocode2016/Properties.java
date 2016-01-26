f36fc8298bbfe2511119b0086c86f15fb07cb39b
755325 <755325@apps.district196.org>
2016-01-25 20:00:07 -0600

    New properties class

---
+++ robocode2016/src/robocode2016/Properties.java
@@ -0,0 +1,14 @@
+package robocode2016;
+
+public class Properties {
+	
+	public static Speedcontroller LEFT_MOTOR = new Jaguar(0);
+	public static Speedcontroller RIGHT_MOTOR = new Jaguar(1);
+	
+	public static Joystick LEFT_JOYSTICK = new Joystick(0);
+	public static Joystick RIGHT_JOYSTICK = new Joystick(1);
+	
+	public static Encoder LEFT_DRIVE = new Encoder(2,3,false,CounterBase.EncodingType.k4x);
+	public static Encoder RIGHT_DRIVE = new Encoder(4,5,false,CounterBase.EncodingType.k4x);
+
+}
